0e75b074692f9675437ea31a314e0ef9aad99c21
NoFate35 <vsokol35@mail.ru>
2025-11-08 20:18:23 +0300

    Changes to be committed: 	modified:   django_orm/libruary/models.py

--- django_orm/libruary/models.py
+++ django_orm/libruary/models.py
@@ -7,17 +7,20 @@ class Author(models.Model):
     country = models.CharField(max_length=50)
 
     # BEGIN (write your solution here)
-    
+    @classmethod
+    def get_author_borrow_stats(self):
+        return Author.objects.annotate(total_borrows = Count('books__borrows'))
 # END
 
 
 class Genre(models.Model):
     name = models.CharField(max_length=50, unique=True)
     # BEGIN (write your solution here)
-    def get_top_genres():
-        genres = Book.objects.prefetch_related("genres", 'borrows').values("genres__name").annotate(borrow_count = Count('borrows')).order_by('-borrow_count')
-        print("gggenres", genres)
-        return genres
+    @classmethod
+    def get_top_genres(self):
+        return Genre.objects.annotate(
+            count_books = Count('' \
+            'books__borrows')).order_by('-count_books')[:3]
 # END
 
 
@@ -28,21 +31,21 @@ class Book(models.Model):
     copies_available = models.PositiveIntegerField(default=1)
 
     # BEGIN (write your solution here)
-    def get_available_books():
-        return Book.objects.filter(copies_available__gt=0)
+    @classmethod
+    def get_available_books(self):
+        return self.objects.filter(copies_available__gt=0)
 
-
+    @transaction.atomic
     def borrow(cls, user):
-        with transaction.atomic():
-            try:
-                Book.objects.filter(id=cls.id).update(copies_available=(cls.copies_available - 1))
-            except:
-                raise ValueError("No copies available")
-            Borrow.objects.create(book=cls, user=user)
+        if cls.copies_available == 0:
+            raise ValueError("No copies available")
+        cls.copies_available -= 1
+        cls.save(update_fields=['copies_available'])
+        Borrow.objects.create(book=cls, user=user)
 
-
-    def get_popular_books():
-        return Book.objects.prefetch_related('borrows').annotate(borrow_count = Count('borrows')).order_by('-borrow_count')
+    @classmethod
+    def get_popular_books(self):
+        return self.objects.annotate(borrow_count = Count('borrows')).order_by('-borrow_count')
     # END 
 
 
@@ -64,6 +67,10 @@ class Review(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="reviews")
     rating = models.PositiveSmallIntegerField(choices=[(i, i) for i in range(1, 6)])
     # BEGIN (write your solution here)
-
+    @classmethod
+    def get_top_rated_books(self, min_reviews, limit):
+        return (Book.objects.annotate(
+            reviews_count = Count('reviews'), 
+            avg_rating = Avg('reviews__rating')).filter(reviews_count__gte = min_reviews).order_by('-avg_rating')[:limit])        
     
 # END
