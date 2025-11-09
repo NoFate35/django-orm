65876be191770c03cd3ffdb05655060f9fe6371a
NoFate35 <vsokol35@mail.ru>
2025-11-09 16:10:26 +0300

    Changes to be committed: 	new file:   django_orm/quote/migrations/0002_quote_approved.py 	modified:   django_orm/quote/models.py

--- django_orm/quote/models.py
+++ django_orm/quote/models.py
@@ -10,12 +10,15 @@ class Quote(models.Model):
     text = models.TextField()
     source = models.CharField(max_length=200)
     # BEGIN (write your solution here)
-    
+    approved = models.BooleanField(default=False)
     # END
 
     @classmethod
     @transaction.atomic
     def of_the_day(cls, key=random_one):
         # BEGIN (write your solution here)
-        pass
+        if all(quote.approved for quote in Quote.objects.all()):
+            Quote.objects.update(approved=False)
+        count = Quote.objects.filter(approved=False).count()
+        print('yyyy', count)
         # END
